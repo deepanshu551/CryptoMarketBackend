a03e52855697980da2cb34ec6da53d20825b384c
deepanshu551 <deepanshusharma551@gmail.com>
2024-04-19 13:20:12 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/MyUser").then(()=>{
+mongoose.connect(process.env.DATABASE).then(()=>{
 
     console.log("db connected")
 }).catch((e)=>{
